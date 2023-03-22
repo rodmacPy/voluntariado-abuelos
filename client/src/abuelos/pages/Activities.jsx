c60a68a4987d3ba4e21052662aa682e5fb725b8b
AdrianCM21 <adriancasco2002@gmail.com>
2023-03-22 14:34:33 -0300

    Mini correcion actividad

--- client/src/abuelos/pages/Activities.jsx
+++ client/src/abuelos/pages/Activities.jsx
@@ -15,25 +15,37 @@ export const Activities = () => {
         GetData();
 
     }, [])
-    function handleParticipar(nombreActividad) {
-        alert(`¡Me gustaría participar en ${nombreActividad}!`);
+    const handleParticipar = async (idActi)=> {
+        try {
+          e.preventDefault();
+          const respuesta = axios.put(`http://localhost:8080/api/actividad/${idActi}`,{
+            headers: {
+                'x-token': `${token}`
+            }
+            
+        })
+        console.log(respuesta)
+        } catch (error) {
+          
+        }
       }
+      console.log(actividades)
     return (
         <>
-<div className='container'>
-  {actividades.map((actividad, index) =>
-    <div className="card" key={index}>
-      <img src={`http://localhost:8080/api/uploads/actividad/${actividad?.uid}`} style={{ "width": '100%' }} />
-      <div className="container">
-        <h3>{actividad.nombre}</h3>
-        <h4>{actividad.descripcion}</h4>
-        <div className="button-container">
-          <button className="participar-btn" onClick={() => handleParticipar(actividad.nombre)}>Participar</button>
+        <div className='container'>
+          {actividades.map((actividad, index) =>
+            <div className="card" key={index}>
+              <img src={`http://localhost:8080/api/uploads/actividad/${actividad.img}`} style={{ "width": '100%' }} />
+              <div className="container">
+                <h3>{actividad.nombre}</h3>
+                <h4>{actividad.descripcion}</h4>
+                <div className="button-container">
+                  <button className="participar-btn" onClick={() => handleParticipar(actividad._id)}>Participar</button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
-    </div>
-  )}
-</div>
         </>
     )
 
